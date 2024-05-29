8727c156405ae850b4feadf4d20a556189f337b2
Interreferences <EKZOTuBe@gmail.com>
2024-05-29 14:57:38 +0400

    Added roles

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,6 @@
-import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {BelongsToMany, Column, DataType, Model, Table} from "sequelize-typescript";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles.model";
 
 interface UserCreationAttrs{
     login:string;
@@ -21,12 +23,18 @@ export class User extends Model<User, UserCreationAttrs>{
     @Column({type:DataType.STRING, allowNull:false})
     password:string;
 
-    @Column({type:DataType.STRING, allowNull:false, defaultValue:"Avatar"})
+    @Column({type:DataType.STRING, allowNull:false, defaultValue:"Avatar.jpg"})
     avatar:string;
+
+    @Column({type: DataType.BOOLEAN, allowNull: false, defaultValue: false})
+    explicit_content:boolean;
 
     @Column({type:DataType.BOOLEAN, allowNull:false, defaultValue:false})
     banned:boolean;
 
-    @Column({type:DataType.STRING, allowNull: false, defaultValue: ""})
+    @Column({type:DataType.STRING, allowNull: true})
     bannedReason:string;
+
+    @BelongsToMany( () => Role, () => UserRoles)
+    roles:Role[];
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@nestjs/common';
 import {User} from "./users.model";
 import {InjectModel} from "@nestjs/sequelize";
 import {CreateUserDto} from "./dto/create-user.dto";
+import {RolesService} from "../roles/roles.service";
 
 @Injectable()
 export class UsersService {
 
-    constructor(@InjectModel(User) private userRepository: typeof User) {}
+    constructor(@InjectModel(User) private userRepository: typeof User,
+                private roleService: RolesService) {}
 
     async CreateUser(dto: CreateUserDto) {
         const user = await this.userRepository.create(dto);
+        const role = await this.roleService.getRoleByValue("User");
+        await user.$set('roles', [role.id]);
         return user;
     }
 
     async GetAllUsers() {
-        const users = await this.userRepository.findAll();
+        const users = await this.userRepository.findAll({include: {all:true}});
         return users;
     }
 

4ad34e1a933557b8cdaebbc5493c11d58af4baf8
Interreferences <EKZOTuBe@gmail.com>
2024-05-31 00:20:23 +0400

    added guard for endpoints, add roles and ban users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,10 @@ import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
 import {CreateUserDto} from "./dto/create-user.dto";
 import {UsersService} from "./users.service";
 import {JwtAuthGuard} from "../auth/jwt-auth.guard";
+import {Roles} from "../auth/roles-auth.decorator";
+import {RolesGuard} from "../auth/roles.guard";
+import {AddRoleDto} from "./dto/add-role.dto";
+import {BanUserDto} from "./dto/ban-user.dto";
 
 @Controller('users')
 export class UsersController {
@@ -14,10 +18,28 @@ export class UsersController {
 
     }
 
-    @UseGuards(JwtAuthGuard)
+
+    // @UseGuards(JwtAuthGuard)
+    @Roles("Admin")
+    @UseGuards(RolesGuard)
     @Get()
     getAll() {
         return this.UsersService.GetAllUsers();
+    }
+
+
+    @Roles("Admin")
+    @UseGuards(RolesGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto) {
+        return this.UsersService.addRole(dto);
+    }
+
+    @Roles("Admin")
+    @UseGuards(RolesGuard)
+    @Post('/ban')
+    ban(@Body() dto: BanUserDto) {
+        return this.UsersService.ban(dto);
     }
 
 }
